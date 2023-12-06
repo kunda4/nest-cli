26e505a5243ff3daf2af5aa47fb58af4bd39e43e
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-06 18:23:04 +0200

    work on find one

--- src/messages/messages.repostory.ts
+++ src/messages/messages.repostory.ts
@@ -2,7 +2,10 @@
 import { readFile, writeFile } from "fs/promises";
 export class MessagesRepostory {
   async findOne(id: string) {
-    
+    const contents = await readFile("messages.json", "utf8")
+    const content = JSON.parse(contents)
+    return content[id]
+
   }
 
   async findAll() {
