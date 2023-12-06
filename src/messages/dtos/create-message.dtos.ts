142eccf10ed50b21b33f706553746a68413b628b
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-06 18:14:05 +0200

    add the messages repostory

--- src/messages/dtos/create-message.dtos.ts
+++ src/messages/dtos/create-message.dtos.ts
@@ -1,4 +1,7 @@
 /* eslint-disable prettier/prettier */
+import {IsString} from 'class-validator'
+
 export class createMessageDtos {
+  @IsString()
   content: string;
 }
---
+++ src/messages/messages.repostory.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { readFile, writeFile } from "fs/promises";
+export class MessagesRepostory {
+  async findOne(id: string) {
+    
+  }
+
+  async findAll() {
+
+  }
+
+  async create(messages: string){
+
+  };
+}
