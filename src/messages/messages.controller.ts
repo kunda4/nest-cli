c3736c3c19a65d1af5541056a724d2cb3c9386bc
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-06 19:49:15 +0200

    add messages.services to controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { createMessageDtos } from './dtos/create-message.dtos';
+import { MessagesServices } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
+  messageServise: MessagesServices;
+  constructor() {
+    this.messageServise = new MessagesServices();
+  }
   @Get()
-  listMessage() {}
+  listMessage() {
+    return this.messageServise.findAll();
+  }
   @Post()
   createMessage(@Body() body: createMessageDtos) {
-    console.log(body);
+    return this.messageServise.create(body.content);
   }
   @Get('/:id')
   getMessage(@Param() id: string) {
-    console.log(id);
+    return this.messageServise.findOne(id);
   }
 }
