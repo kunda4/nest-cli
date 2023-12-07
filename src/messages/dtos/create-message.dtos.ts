988cd72ea688702059ed2b850dd80370956aceb8
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-07 12:33:03 +0200

    fix providers

--- src/messages/dtos/create-message.dtos.ts
+++ src/messages/dtos/create-message.dtos.ts
@@ -1,10 +1,9 @@
 /* eslint-disable prettier/prettier */
 import {IsString} from 'class-validator'
-import { MessagesServices } from '../messages.services';
-import { MessagesRepository } from '../messages.repostory';
+
 
 export class createMessageDtos {
   @IsString()
   content: string;
-  provider: [MessagesServices, MessagesRepository]
+
 }
--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesServices } from './messages.services';
+import { MessagesRepository } from './messages.repostory';
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessagesServices, MessagesRepository],
 })
 export class MessagesModule {}
