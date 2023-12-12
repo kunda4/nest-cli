682e63a69798c1b9db76a40e49151a010f18cf9f
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-12 16:15:41 +0200

    work on delete functionality

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -31,25 +31,13 @@ export class MessagesController {
     return message;
   }
   @Delete('/:id')
-  // async deleteMessage(@Param('id') id: string) {
-  //   const message = await this.messageService.deleteOne(id);
-  //   if (!message) {
-  //     throw new NotFoundException('message not found!');
-  //   }
-  //   return { message: 'deleted successfuly' };
-  // }
   async deleteMessage(@Param('id') id: string) {
     const contents = await readFile('messages.json', 'utf8');
     const content = JSON.parse(contents);
-
     if (!content[id]) {
       throw new NotFoundException(`Record with ID ${id} not found`);
     }
-
-    // Remove the entry associated with the specified ID
     delete content[id];
-
-    // Write the modified content back to the file
     await writeFile('messages.json', JSON.stringify(content, null, 2));
 
     return { message: `Record with ID ${id} deleted successfully` };
